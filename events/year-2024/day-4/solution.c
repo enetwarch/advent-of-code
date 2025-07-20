03d54ae67a12a0bee30fab89c93ab83cf4f51a7f
enetwarch <enetwarch@protonmail.com>
2025-07-20 19:59:12 +0800

    feat: completed 2024 day 4 part 2

--- events/year-2024/day-4/solution.c
+++ events/year-2024/day-4/solution.c
@@ -9,12 +9,13 @@
 #define COLS 140
 
 #define PART_ONE_ANSWER 2549
-#define PART_TWO_ANSWER 0
+#define PART_TWO_ANSWER 2003
 
 int y2024_d04_p1(char *file_name, int rows, int cols);
 int y2024_d04_p2(char *file_name, int rows, int cols);
 
 int count_surrounding_xmas(char *input, int index, int rows, int cols);
+bool is_cross_mas(char *input, int index, int rows, int cols);
 bool is_valid_step(int from_index, int to_index, int rows, int cols);
 char *parse_file(char *file_name, int rows, int cols);
 
@@ -53,11 +54,25 @@ int y2024_d04_p1(char *file_name, int rows, int cols) {
 int y2024_d04_p2(char *file_name, int rows, int cols) {
     char *input = parse_file(file_name, rows, cols);
 
+    int cross_mas_count = 0;
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            int index = (row * cols) + col;
+            char character = input[index];
+
+            if (character != 'A') continue;
+            if (!is_cross_mas(input, index, rows, cols)) continue;
+
+            cross_mas_count++;
+        }
+    }
+
     free(input);
-    return 0;
+    return cross_mas_count;
 }
 
 // Small directional calculator functions.
+
 int n(int index, int cols) { return index - cols; }
 int ne(int index, int cols) { return index - cols + 1; }
 
@@ -73,6 +88,7 @@ int nw(int index, int cols) { return index - cols - 1; }
 
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
 int count_surrounding_xmas(char *input, int index, int rows, int cols) {
+    if (input[index] != 'X') return 0;
     int (*directions[8])(int, int) = {n, ne, e, se, s, sw, w, nw};
 
     char xmas_string[] = "XMAS";
@@ -101,6 +117,35 @@ int count_surrounding_xmas(char *input, int index, int rows, int cols) {
     return xmas_count;
 }
 
+bool is_cross_mas(char *input, int index, int rows, int cols) {
+    if (input[index] != 'A') return false;
+
+    // By making the array this order, it checks opposite ends first.
+    int (*directions[4])(int, int) = {ne, sw, nw, se};
+
+    int m_character_count = 0;
+    int s_character_count = 0;
+    for (size_t i = 0; i < (sizeof(directions) / sizeof(directions[0])); i++) {
+        int (*direction)(int, int) = directions[i];
+
+        int next_index = direction(index, cols);
+        if (!is_valid_step(index, next_index, rows, cols)) return false;
+
+        char character = input[next_index];
+        if (character != 'M' && character != 'S') return false;
+        if (character == 'M') m_character_count++;
+        if (character == 'S') s_character_count++;
+
+        // The directions are being checked by the opposite ends first.
+        // If two ends are the same character, that means it's either MAM or SAS.
+        // We are looking for MAS only, and this conditional handles that.
+        if (abs(m_character_count - s_character_count) > 1) return false;
+    }
+
+    return (m_character_count == 2 && s_character_count == 2);
+}
+
+// Makes sure the next index is within the 2d array boundary.
 bool is_valid_step(int from_index, int to_index, int rows, int cols) {
     if (from_index == to_index) return true;
     if (to_index < 0 || to_index >= (rows * cols)) return false;
@@ -128,7 +173,7 @@ char *parse_file(char *file_name, int rows, int cols) {
 
     char buffer[cols + 2];
     for (int row = 0; row < rows; row++) {
-        fgets(buffer, sizeof(buffer), file);
+        fgets(buffer, (int)sizeof(buffer), file);
 
         for (int col = 0; col < cols; col++) {
             input[(row * cols) + col] = buffer[col];
