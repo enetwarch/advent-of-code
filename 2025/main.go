02d4bd0e52498f809625bb4c1729e1e0a8c43541
enetwarch <enetwarch@protonmail.com>
2025-12-06 01:04:35 +0800

    refactor: removed day_ prefix in files

--- 2025/main.go
+++ 2025/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Printf("Year 2025 Day 1 Part 1: %d\n", soln.Y2025D1P1("./input/day_1.txt"))
-	fmt.Printf("Year 2025 Day 1 Part 2: %d\n", soln.Y2025D1P2("./input/day_1.txt"))
+	fmt.Printf("Year 2025 Day 1 Part 1: %d\n", soln.Y2025D1P1("./input/1.txt"))
+	fmt.Printf("Year 2025 Day 1 Part 2: %d\n", soln.Y2025D1P2("./input/1.txt"))
 
-	fmt.Printf("Year 2025 Day 2 Part 1: %d\n", soln.Y2025D2P1("./input/day_2.txt"))
-	fmt.Printf("Year 2025 Day 2 Part 2: %d\n", soln.Y2025D2P2("./input/day_2.txt"))
+	fmt.Printf("Year 2025 Day 2 Part 1: %d\n", soln.Y2025D2P1("./input/2.txt"))
+	fmt.Printf("Year 2025 Day 2 Part 2: %d\n", soln.Y2025D2P2("./input/2.txt"))
 }
